818bd69b9ccb3b80226beb7083b70072d0e5f4c5
달쿠비임당 <dydvy08@gmail.com>
2025-03-17 14:23:14 +0900

    2

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("---------------------------------\n");
-    printf("           학과: 컴퓨터 공학과\n");
-    printf("           학번; 20251484\n");
-    printf("           성명: 김용표\n");
-    printf("---------------------------------\n");
+    printf("Hello, World!\n");
     return 0;
 }
